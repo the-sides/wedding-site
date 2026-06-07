341ae70d9a00cbbd14e1400a898511c012c97ef7
Jacob Sides <jacob.r.sides@gmail.com>
2026-06-07 17:37:56 -0400

    Vercel support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
@@ -19,7 +20,10 @@ const config = defineConfig({
         failOnError: false, // Don't fail build on prerender errors (nitro-nightly should fix race condition)
         retryCount: 3,
         retryDelay: 1000,
-      }
+      },
+    }),
+    nitro({
+      preset: 'vercel',
     }),
     viteReact(),
   ],
